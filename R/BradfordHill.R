e7dea40b0ea2e4cafe6bfbddc8ef1423108de8d8
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2017-07-21 10:26:00 +0100

    Add files via upload

---
+++ R/BradfordHill.R
@@ -0,0 +1,17 @@
+# BradfordHill.R
+# Implements aspects of the nine Bradford-Hill criteria for biological plausibility and coherence.
+
+# 1. Strength of the association, the stronger the association the more likely that the association is causal.
+# 2. Consistency , if more studies find similar results, the more likely it is that the association is causal.
+# 3. Specificity, a specific exposure should exert a specific effect. There are causal associations that are not
+#   specific, for example, cigarette smoking,  which are associated with multiple carcino-genic effects.
+# 4. Temporality, the causal exposure should precede the caused disease in time.
+# 5. Biological gradient or dose response. If is seen then more likely that the association is causal. 
+# 6. Plausibility depends on the current knowledge of the etiology of the disease. For instance, is it known that
+# the agent or metabolite reaches the target organ, are studies in animal models positive?
+# 7. Coherence, refers to other observed biological ef  logical changes in the target organ.
+# 8. Experimental evidence,if the disease rates go down after the causal agent has been eliminated, it is sup-
+#  port for a causal association.
+# 9. Analogy, if a similar agent exerts similar effects, it is more likely that the association is causal.
+
+
