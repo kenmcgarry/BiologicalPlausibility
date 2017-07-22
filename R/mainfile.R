25c0039202a58c146b7fa620842fd2980990f51d
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2017-07-22 10:38:29 +0100

    Add files via upload

---
+++ R/mainfile.R
@@ -0,0 +1,19 @@
+# mainfile.R
+# July 2017
+
+library(infotheo)
+#library(entropy)      # discretize() is over written by entropy packages - see warning.
+#library(Information)  # weight of evidence (WOE) and information value (IV)
+
+library(dplyr)
+library(tidyr)
+library(xtable)
+library(ggplot2)
+
+library(clusterProfiler)
+library(ClustOfVar)
+library(ReactomePA)
+library(igraph)
+library(minet)
+library(linkcomm) 
+
