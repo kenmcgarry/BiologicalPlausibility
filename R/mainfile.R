b28e78606e477db63d96b31f5ad2342f833ae449
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2017-07-26 08:25:21 +0100

    Add files via upload

--- R/mainfile.R
+++ R/mainfile.R
@@ -17,31 +17,32 @@ library(igraph)
 library(minet)
 library(linkcomm) 
 
-# load in protein data
+# load in gene data via the packages
 library(care)
-library("GeneCycle")
-library("qvalue")
+library(GeneCycle)
+library(qvalue)
+library(GeneNet)
 
 # Lu et al. (2004) brain aging gene expression data: see lu2004 data.
-data("lu2004")
-dim(lu2004$x)
+#data("lu2004")
+#dim(lu2004$x)
 
-# http://www.pnas.org/content/98/20/11462.abstract]
+# http://www.pnas.org/content/98/20/11462.abstract
 # West et al. (2001) breast cancer data: westdataclean.rda.gz.
+load("C:\\R-files\\dataCANCER\\westdataclean.rda")
 
 # http://www.nejm.org/doi/full/10.1056/NEJM200102223440801
 # Hedenfalk et al. (2001) breast cancer data: hedenfalk.rda.gz.
+load("C:\\R-files\\dataCANCER\\hedenfalk.rda")
 
 # http://www.molbiolcell.org/content/13/6/1977.abstract
 # Whitfield et al. (2002) human HeLa cell-cycle data: humanhela.rda.gz.
 load("C:\\R-files\\dataCANCER\\humanhela.rda")
 
-dim(score1)      # 12 x 14728
-dim(score2)      # 26 x 15472
-dim(score3)      # 48 x 39724
-dim(score4)      # 19 x 39192
-dim(score5)      #  9 x 34890
-
+# Smith et al. (2004) A. thaliana time series data:
+#  the  temporal  expression  of  800  genes  of A.thaliana during  the  diurnal cycle.  
+data("arth800")  # from GeneNet package
+summary(arth800.expr)
 
 
 
